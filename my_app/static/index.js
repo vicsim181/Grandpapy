ed96ce5c30c5d30d48d3608f632de2453e55e48e
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-02-09 18:50:34 +0100

    update index.js

--- my_app/static/index.js
+++ my_app/static/index.js
@@ -83,7 +83,8 @@ async function displayMessages() {
 }
 
 
-$("#button").on('click', async function () {
+$("#button").on('click', async function (event) {
+    event.preventDefault();
     $('#Layer_1').show();
     await displayMessages();
     setTimeout(() => {
